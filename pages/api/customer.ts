c112f08ea94395d4800d26beace4b6959f75c38f
nickderaj <nickderaj@gmail.com>
2022-07-16 12:01:33 +0800

    feat: add the subscriptions data to the customer api

--- pages/api/customer.ts
+++ pages/api/customer.ts
@@ -10,6 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     const customer = await stripe!.customers.search({
       query: `email: '${email}'`,
+      expand: ['data.subscriptions'],
     });
     if (customer.data.length === 0) res.status(200).json({ error: 'Nothing found.' });
 
